bae6bc549ccff06855bf05f35abc291a8bff34eb
javascriptframework <javascripttraining@outlook.com>
2022-01-26 07:31:45 -0600

    first commit

--- src/Component/Context/ContextFile.js
+++
@@ -1,4 +0,0 @@
-import { createContext} from 'react';
-export const CourseContext = createContext();
-//export const CourseContextProvider = CourseContext.Provider;
-export const CourseContextConsumer = CourseContext.Consumer;
